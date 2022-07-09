cd146ac4c6c5808cf6506f6c68166db288f7a0d9
Anuel AA <105551679+RHLM-SCRIPTS@users.noreply.github.com>
2022-07-09 21:33:25 +0200

    Add files via upload

---
+++ rhlm_blips/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author '😈 𝐀𝐍𝐔𝐄𝐋 𝐀𝐀 😈#6979'
+
+description 'rhlm_blips Pon tus blips a tu gusto y con el comando /blip escondelos o muestralo (Solo lo verás tu escondido), es para una mejor jugabilidad visual del mapa para los jugadores.'
+
+client_script {
+    'client/*.lua'
+}
